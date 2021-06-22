97fab0f7762431e577c48073e75cdbfa2446b2bb
Kun Chen <chenkun@mail.ustc.edu.cn>
2021-06-22 11:39:16 -0400

    wip

--- src/exact.jl
+++ src/exact.jl
@@ -4,6 +4,7 @@ const Float = Float64
 const Vec = Vector{Tuple{Float,Float}}
 const rank = 16
 const atol = 1.0e-10
+const Λ = Float(1000)
 
 # """
 # \int_1^∞ e^{-ω_1 τ}*e^{-ω_2*τ} dτ = 1/(ω_1+ω_2)
@@ -40,12 +41,34 @@ end
 Norm(ω) = sqrt(proj(ω, ω))
 
 function Norm(freq, Q, ω::Float)
+#   qi=sum_j c_ij e^{-ω_j*τ}
+#   norm2 = 1/2ω- \sum_i c_ij*c_ik/(ω+ω_j)/(ω+ω_k)
     norm2 = proj(ω, ω)
     for (qi, q) in enumerate(Q)
         norm2 -= (proj(freq, q, ω))^2
     end
     @assert norm2 > 0
     norm = sqrt(norm2)
+end
+
+"""
+First derivative of norm2 for the vector: e^{-ωτ}-sum_i <q_i, e^{-ωτ}>
+"""
+function DNorm2(freq, Q, ω::Float)
+#   qi=sum_j c_ij e^{-ω_j*τ}
+#   dnorm2 = -1/2ω^2 + \sum_{jk} (\sum_i c_ij*c_ik)*(2ω+ω_j+ω_k)/(ω+ω_j)^2/(ω+ω_k)^2
+    dnorm2 = -1 / (2 * ω^2)
+    # println("omega:", ω, ", ", proj(ω, ω))
+    for j in 1:length(Q)
+        for k in 1:length(Q)
+            amp = Float(0)
+            for i in 1:length(Q)
+                amp += Q[i][j] * Q[i][k]
+            end
+            dnorm2 += amp * (2 * ω + freq[j] + freq[k]) / (ω + freq[j])^2 / (ω + freq[k])^2
+        end
+    end
+    return dnorm2
 end
 
 # function NormExpr(freq, Q)
@@ -92,8 +115,18 @@ if abspath(PROGRAM_FILE) == @__FILE__
     println(Q[1])
     println(Norm(freq, Q, 2.0))
 
+    # println("Derivative: ", DNorm2(freq, Q, 1.0))
+    # println("Derivative: ", DNorm2(freq, Q, 2.0))
+    # println("Derivative: ", DNorm2(freq, Q, 10.0))
+    # println("Derivative: ", DNorm2(freq, Q, 100.0))
+
     freq[2] = 2.0
     push!(Q, orthognalize(freq, Q, 2.0))
     testOrthgonal(freq, Q)
+
+    # println("Derivative: ", DNorm2(freq, Q, 2.0))
+    # println("Derivative: ", DNorm2(freq, Q, 10.0))
+
+
 
 end
